5117d3e7de26e80cf30d4b7baac3c2f1da237ff7
hotshot1799 <ganeshgunaki.1798@gmail.com>
2024-11-17 02:55:45 +0000

    Update models.py

--- models.py
+++ models.py
@@ -16,8 +16,6 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    def __repr__(self):
-        return f'<User {self.username}>'
 
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
