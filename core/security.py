601be733bcd2bab6c840dc4efa0d857213256130
hotshot1799 <ganeshgunaki.1798@gmail.com>
2024-11-22 02:31:44 +0000

    Update security.py

--- core/security.py
+++ core/security.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Any, Union, Optional
 from jose import jwt
 from passlib.context import CryptContext
-from app.core.config import settings
+from core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
