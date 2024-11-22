814501d953cf57e5eded3ba10946b0aa0f44b69c
hotshot1799 <ganeshgunaki.1798@gmail.com>
2024-11-22 02:32:10 +0000

    Update goal.py

--- models/goal.py
+++ models/goal.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from app.database import Base
+from database import Base
 
 class Goal(Base):
     __tablename__ = "goals"
