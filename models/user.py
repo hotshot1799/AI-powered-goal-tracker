42a4fb82615061ce7c562a78d85dbeb2b2c8397e
hotshot1799 <ganeshgunaki.1798@gmail.com>
2024-11-23 01:01:12 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -13,7 +13,8 @@ class User(Base):
     hashed_password = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
     
-    goals = relationship("Goal", back_populates="user")
+    # Define relationship
+    goals = relationship("Goal", back_populates="user", cascade="all, delete-orphan")
 
     def verify_password(self, password: str) -> bool:
         return verify_password(password, self.hashed_password)
