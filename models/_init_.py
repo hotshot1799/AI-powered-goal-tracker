13241317248ae1ab4cab9f56a02eaf6d2d2d09bf
hotshot1799 <ganeshgunaki.1798@gmail.com>
2024-11-23 01:09:48 +0000

    Delete models directory

--- models/_init_.py
+++
@@ -1,6 +0,0 @@
-from models.user import User
-from models.goal import Goal
-from models.progress import ProgressUpdate
-
-# This resolves circular imports
-__all__ = ['User', 'Goal', 'ProgressUpdate']
