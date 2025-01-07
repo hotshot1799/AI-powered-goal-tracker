97355cacdaf146d599407d596acafd10f31ff5c1
hotshot1799 <ganeshgunaki.1798@gmail.com>
2025-01-07 01:51:19 +0000

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
     build: {
       sourcemap: true,
     },
-  });
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Maps "@" to "src" folder
+    },
+  },
+});
